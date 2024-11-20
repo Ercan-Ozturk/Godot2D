69c8fe769f0cb87dcbcd4e4e860a3592c9d0bfb2
Ercan Öztürk <ercanozturk788@gmail.com>
2024-11-20 13:26:07 +0300

    Light and shadow

---
+++ movable_object.gd
@@ -0,0 +1,25 @@
+class_name MovableObject
+extends CharacterBody2D
+@export_range(0.0, 10.0) var drag := 5.0
+@export_range(0.0, 1.0) var impact_response := 0.5
+
+func _physics_process(delta: float) -> void:
+	if velocity.length_squared() > 1.0:
+		velocity *= 1.0 - drag * delta
+	if move_and_slide():
+		resolve_collisions()
+	else:
+		position = round(position)
+		velocity = Vector2.ZERO
+
+func resolve_collisions() -> void:
+	for i in get_slide_collision_count():
+		var collision := get_slide_collision(i)
+		var body := collision.get_collider() as MovableObject
+		if body:
+			body.apply_impact(velocity)
+		else:
+			velocity -= velocity.project(collision.get_normal())
+		
+func apply_impact(impact_velocity: Vector2) -> void:
+	velocity += (impact_velocity - velocity) * impact_response
